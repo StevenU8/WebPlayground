eca74054d7c8443c400a8a2963a7719491c50350
Steven U <su@paraport.com>
2015-05-15 13:02:47 -0700

    Added a couple versions for upload screen

--- WebPlayground/WebPlayground/App_Start/RouteConfig.cs
+++ WebPlayground/WebPlayground/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebPlayground
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Allocation", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- WebPlayground/WebPlayground/Controllers/AllocationController.cs
+++ WebPlayground/WebPlayground/Controllers/AllocationController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 using FizzWare.NBuilder;
 using Microsoft.SqlServer.Server;
 
@@ -18,14 +19,72 @@ namespace WebPlayground.Controllers
 
         public ActionResult Upload()
         {
-            var allocations = Builder<Allocation>
-                .CreateListOfSize(1000)
-                .All().Do(a => a.AccountId = ((int)a.Weight % 20).ToString())
-                .All().Do(a => a.Message = (int)a.Weight % 8 == 0 ? "Validation Error" : "")
-                .Build()
-                .AsEnumerable();
+            var allocations = GetFakeAllocations();
 
             return View(allocations);
+        }
+        
+        public ActionResult Upload2()
+        {
+            var allocations = GetFakeAllocations().ToList();
+
+            var accountAllocations = allocations
+                .GroupBy(a => a.AccountId).Select(b =>
+                {
+                    return new AccountAllocation
+                    {
+                        AccountId = b.Key,
+                        HasErrors =
+                            allocations.Where(a => a.AccountId == b.Key).Any(a => !a.ValidationMessage.IsEmpty())
+                    };
+                });
+                    
+            var allocationViewModel = new AllocationViewModel2
+            {
+                AccountAllocations = accountAllocations,
+                Allocations =  allocations
+            };
+
+            return View(allocationViewModel);
+        }
+        
+        public ActionResult Upload3()
+        {
+            var allocations = GetFakeAllocations().ToList();
+
+            var accountAllocations = allocations
+                .GroupBy(a => a.AccountId).Select(b =>
+                {
+                    return new AccountAllocation
+                    {
+                        AccountId = b.Key,
+                        HasErrors = allocations.Where(a => a.AccountId == b.Key).Any(a => !a.ValidationMessage.IsEmpty())
+                    };
+                })
+                .ToList();
+
+            var validAccountIds = accountAllocations.Where(a => !a.HasErrors).Select(a => a.AccountId);
+            var invalidAccountIds = accountAllocations.Where(a => a.HasErrors).Select(a => a.AccountId);
+
+
+
+            var allocationViewModel = new AllocationViewModel3
+            {
+                ValidAllocations = allocations.Where(a => validAccountIds.Contains(a.AccountId)),
+                InvalidAllocations = allocations.Where(a => invalidAccountIds.Contains(a.AccountId)),
+            };
+
+            return View(allocationViewModel);
+        }
+
+        private static IEnumerable<Allocation> GetFakeAllocations()
+        {
+            return Builder<Allocation>
+                .CreateListOfSize(1000)
+                .All().Do(a => a.AccountId = (((int)a.Weight % 20 + 1) * 12345).ToString())
+                .All().Do(a => a.ValidationMessage = (int)a.Weight % 8 == 0 ? "Validation Error" : "")
+                .Build()
+                .AsEnumerable();
         }
 
         public ActionResult Process()
@@ -41,6 +100,24 @@ namespace WebPlayground.Controllers
         public string Model { get; set; }
         public double Weight { get; set; }
         public bool PreserveDrift { get; set; }
-        public string Message { get; set; }
+        public string ValidationMessage { get; set; }
+    }
+
+    public class AllocationViewModel2
+    {
+        public IEnumerable<AccountAllocation> AccountAllocations { get; set; }
+        public IEnumerable<Allocation> Allocations { get; set; }
+    }
+
+    public class AllocationViewModel3
+    {
+        public IEnumerable<Allocation> ValidAllocations { get; set; }
+        public IEnumerable<Allocation> InvalidAllocations { get; set; }
+    }
+
+    public class AccountAllocation
+    {
+        public string AccountId { get; set; }
+        public bool HasErrors { get; set; }
     }
 }
